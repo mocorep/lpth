4eb3fdb4559da73b1e1d7570d962bdfabeb2d10f
micke <mico02@kth.se>
2015-06-16 20:36:07 +0530

    initial commit

---
+++ ex5.py
@@ -0,0 +1,3 @@
+my_number = 17
+my_name = "mocorep"
+print "A number here '%d' and a string here '%s', where will this end.!?" % ( my_number, my_name )
---
+++ ex6.py
@@ -0,0 +1,4 @@
+hilarious = False
+joke_evaluation = "Isn't that joke so funny.!? %r"
+
+print joke_evaluation % hilarious
